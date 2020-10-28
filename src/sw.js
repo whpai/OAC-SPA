72c4ffce3506df734be04da059223117f913c93a
rwdev99 <rwdev99@gmail.com>
2020-10-28 11:59:14 +0800

    change spaces to tabs @ sw.js

--- src/sw.js
+++ src/sw.js
@@ -2,70 +2,70 @@
 var assets = null;
 
 self.addEventListener('install', function(evt) {
-    console.log('[Service Worker] is being installed.', ACACHE);
-    //self.skipWaiting();
+	console.log('[Service Worker] is being installed.', ACACHE);
+	//self.skipWaiting();
 
-    try {
-        importScripts("sw-manifest.js");
-        console.log('[Service Worker]pre-fetch', PRECACHE);
-    } catch (e) {
-        // empty list
-        PRECACHE = [];
-    }
+	try {
+		importScripts("sw-manifest.js");
+		console.log('[Service Worker]pre-fetch', PRECACHE);
+	} catch (e) {
+		// empty list
+		PRECACHE = [];
+	}
 
-    try {
-        evt.waitUntil(caches.open(ACACHE).then(function(cache) {
-            var urls = ['./'];
-            PRECACHE.forEach(function(val, idx, arr) {
-                urls.push(val.url);
-            });
-            console.log("[urls]", urls)
-            cache.put('./.assets', new Response(JSON.stringify(urls)));
-            assets = urls;
-            return cache.addAll(urls);
-        }));
-    } catch (e) {
-        console.log('[Service Worker]pre-fetch failed', e);
-    }
+	try {
+		evt.waitUntil(caches.open(ACACHE).then(function(cache) {
+			var urls = ['./'];
+			PRECACHE.forEach(function(val, idx, arr) {
+				urls.push(val.url);
+			});
+			console.log("[urls]", urls)
+			cache.put('./.assets', new Response(JSON.stringify(urls)));
+			assets = urls;
+			return cache.addAll(urls);
+		}));
+	} catch (e) {
+		console.log('[Service Worker]pre-fetch failed', e);
+	}
 });
 
 self.addEventListener('fetch', async function(evt) {
-    var req = evt.request;
-    evt.respondWith(assetsFromCache(req));
-    //evt.respondWith(fetch(req)); // bypass
+	var req = evt.request;
+	evt.respondWith(assetsFromCache(req));
+	//evt.respondWith(fetch(req)); // bypass
 });
 
 async function assetsFromCache(req) {
-    var cache = await caches.open(ACACHE);
-    if (!assets) {
-        var listJson = await cache.match('./.assets');
-        assets = listJson.json();
-    }
+	var cache = await caches.open(ACACHE);
+	if (!assets) {
+		var listJson = await cache.match('./.assets');
+		assets = listJson.json();
+	}
 
-    if (req.url.match(/\/\?.*$/)) { // remove '/?xxxxx...'
-        req = './';
-    }
-    var resp = await cache.match(req) || fetch(req);
-    return resp;
+	if (req.url.match(/\/\?.*$/)) { // remove '/?xxxxx...'
+		req = './';
+	}
+	var resp = await cache.match(req) || fetch(req);
+	return resp;
 }
 
 self.addEventListener('activate', function(ev) {
-    const aCache = ACACHE;
-    const dCache = DCACHE;
-    ev.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.map(function(cacheName) {
-                    if ((aCache != cacheName) && (dCache != cacheName)) {
-                        console.log('Deleting out of date cache:', cacheName);
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+	const aCache = ACACHE;
+	const dCache = DCACHE;
+	ev.waitUntil(
+		caches.keys().then(function(cacheNames) {
+			return Promise.all(
+				cacheNames.map(function(cacheName) {
+					if ((aCache != cacheName) && (dCache != cacheName)) {
+						console.log('Deleting out of date cache:', cacheName);
+						return caches.delete(cacheName);
+					}
+				})
+			);
+		})
+	);
 });
 
 self.addEventListener('message', async function(ev) {
-    if (ev.data === 'skipWaiting') return self.skipWaiting();
+	if (ev.data === 'skipWaiting') return self.skipWaiting();
 });
